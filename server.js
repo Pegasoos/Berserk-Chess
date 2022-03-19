202ae48d23a59b6bbf797f129de04fdf562de454
Pegasoos <circumambulation24@yahoo.com>
2022-03-19 18:17:05 -0400

    Setup MySQL connection in server w/ .env

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
+
+//const helpers = require('./utils/helpers');
+//const hbs = exphbs.create({helpers});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -8,8 +12,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const dotenv = require('dotenv').config()
+
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -20,11 +26,14 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars');
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Serving at http://localhost:${PORT}`));
 });
