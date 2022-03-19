a58a3ccd04e96fee3146026efa99b79305d2e426
Pegasoos <circumambulation24@yahoo.com>
2022-03-19 00:36:12 -0400

    Updated schema, deleted players set

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,19 @@
-DROP DATABASE bchess_db;
+DROP DATABASE IF EXISTS bchess_db;
 CREATE DATABASE bchess_db;
 USE bchess_db;
 CREATE TABLE IF NOT EXISTS user(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     username VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(30) NOT NULL,
-    wins INT DEFAULT 0,
+    wins INT DEFAULT (0)
     );
 CREATE TABLE IF NOT EXISTS matches(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     match_time INT NOT NULL,
-    players SET(),
-    FOREIGN KEY (players) REFERENCES user (username),
-    winner VARCHAR(30)
-    FOREIGN KEY (winner) REFERENCES user (username),
+	player1 VARCHAR(30) NOT NULL,
+    FOREIGN KEY (player1) REFERENCES user (username),
+    player2 VARCHAR(30) NOT NULL,
+    FOREIGN KEY (player2) REFERENCES user (username),
+    winner VARCHAR(30) NOT NULL,
+    FOREIGN KEY (winner) REFERENCES user (username)
 );
