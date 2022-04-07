b404f815569c41c151ecc2d5420dc4ff0b0d5f1a
Pegasoos <circumambulation24@yahoo.com>
2022-04-06 20:49:42 -0400

    Route For Profile View Loads Data Correctly

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Match } = require('../models');
 const withAuth = require('../utils/auth');
-
+const sequelize = require('sequelize');
+const { Op } = sequelize;
 router.get('/', (req, res) =>{
   try{
     res.render('homepage', {logged_in:req.session.logged_in});
@@ -42,9 +43,14 @@ router.get('/leaderboard', async (req, res) =>{
     res.status(500).json(err);
   }
 });
-router.get('/profile', withAuth, (req, res) =>{
+router.get('/profile', withAuth, async (req, res) =>{
   try{
-    res.render('profile',{logged_in:req.session.logged_in});
+    const userData = await User.findOne({where: {username:req.session.username}});
+    const user = userData.get({ plain: true});
+    const matchData = await Match.findAll({where:{[Op.or]:[{player1:req.session.username}, {player2:req.session.username}]}});
+    const matches = matchData.map((match) => match.get({ plain: true }));
+    console.log(matches);
+    res.render('profile', { logged_in:req.session.logged_in, matches, user}); //
   } catch (err){
     res.status(500).json(err);
   }
